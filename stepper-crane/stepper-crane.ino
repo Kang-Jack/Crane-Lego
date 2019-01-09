0673cd638b14dda93a13b2283dbad83635703e5f
Kang <Kang-Jack@users.noreply.github.com>
2019-01-09 23:03:23 +0800

    turning speed

--- stepper-crane/stepper-crane.ino
+++ stepper-crane/stepper-crane.ino
@@ -14,51 +14,84 @@
 
 //#define ledPin 5
 
-int redPin = A0;  // Nano 3 
-int bluePin = A1;  //Nano5
-int greenPin = A2;  //Nano6
+int redPin = A0;  // Nano 3 ,R 红色LED 控制引脚 连接到Arduino的 11脚
+int bluePin = A1;  //Nano5, B蓝色LED 控制引脚 连接到Arduino的 11脚 
+int greenPin = A2;  //Nano6, G绿色LED 控制引脚 连接到Arduino的 9脚
 
 
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
 AccelStepper stepper1(FULLSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
 int steps1 = 0;
-int FWSteps = 1200;
-int BWSteps = -1200;
-int stepperSpeed = 400;
+int FWSteps = 248000;
+int BWSteps = -248000;
+int stepperSpeed = 200;
 int upVal = LOW;
 int downVal = LOW;
-
+bool isFW =false;
 void setup() {
   Serial.begin(9600);
   pinMode(upPin, INPUT); 
   pinMode(downPin, INPUT);
   //pinMode(ledPin, OUTPUT);  
-  pinMode(redPin, OUTPUT); 
-  pinMode(greenPin, OUTPUT); 
-  pinMode(bluePin, OUTPUT); 
+  pinMode(redPin, OUTPUT); //设置redPin对应的管脚11为输出
+  pinMode(greenPin, OUTPUT); //设置greenPin,对应的管脚9为输出
+  pinMode(bluePin, OUTPUT); //设置bluePin对应的管脚10为输出
   
-  stepper1.setMaxSpeed(2000.0);
+  stepper1.setMaxSpeed(1600.0);
+  stepper1.setAcceleration(100.0);
   stepper1.move(1);  // I found this necessary
   stepper1.setSpeed(stepperSpeed);
 
 }//--(end setup )---
 
 void loop() {
-  if (steps1==0){
-    lightDown(); 
+  if (steps1%9 == 0){
     upVal = digitalRead(upPin);
     downVal= digitalRead(downPin);
-    delay (200);
   }
-  if (upVal == downVal && steps1==0){
-    delay (500);
+  if (upVal == downVal){
+    steps1 = stepper1.distanceToGo();
+    //Serial.println(steps1);
+    if(steps1 != 0)
+    {
+      stepper1.move(1);  // I found this necessary
+      stepper1.setSpeed(stepperSpeed);
+      stepper1.runSpeedToPosition();
+    }
+    lightDown(); 
+    isFW =false;
+    delay (50);
     return;
   }
-  if (upVal == HIGH  && steps1==0){    
-      cmdStep(FWSteps); 
+  if (upVal == HIGH ){   
+      if (isFW)
+      {
+         if  (steps1==0)
+            cmdStep(FWSteps); 
+         else
+            cmdStep(0); 
+      }
+      else
+      {
+        steps1 = 0;
+        cmdStep(FWSteps); 
+      }
+      isFW=true;
   }
-  if (downVal == HIGH && steps1==0) {  
-     cmdStep(BWSteps); 
+  if (downVal == HIGH) {  
+      if (!isFW)
+      {
+         if  (steps1==0)
+            cmdStep(BWSteps); 
+         else
+            cmdStep(0); 
+      }
+      else
+      {
+        steps1 = 0;
+        cmdStep(BWSteps); 
+      }
+      isFW=false;
    }
    if (steps1 != 0){
     cmdStep(0); 
@@ -72,7 +105,7 @@ void cmdStep(int steps){
       stepper1.setSpeed(stepperSpeed);
     }
     steps1 = stepper1.distanceToGo();
-    Serial.println(steps1);
+    //Serial.println(steps1);
     stepper1.runSpeedToPosition();   
 }
 void color (unsigned char red, unsigned char green, unsigned char blue) 
@@ -83,17 +116,17 @@ void color (unsigned char red, unsigned char green, unsigned char blue)
 }  
 void lightUp(){
       // Basic colors:  
-      color(0,255, 0);
-      delay(100); 
-      color(0, 0, 255);
-      delay(100); 
-      color(255,255,0);
-      delay(100); 
-      color(255,255,255);
-      delay(100); 
-      color(128,0,255);
-      delay(100);   
-      color(255, 0, 0);
+      color(0,255, 0); //绿色亮
+      delay(50); 
+      color(0, 0, 255); // 蓝色灯亮  
+      delay(50); 
+      color(255,255,0); // 黄色  
+      delay(50); 
+      color(255,255,255); // 白色  
+      delay(50); 
+      color(128,0,255); // 紫色  
+      delay(50);   
+      color(255, 0, 0); // 红色亮 
 }
 void lightDown(){
      // Basic colors:  
